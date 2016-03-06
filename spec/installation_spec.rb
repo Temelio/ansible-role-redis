8e975b450a5f5647ae8133e975df87ed940c0d1c
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-03-06 19:10:01 +0100

    Manage redis installation

--- spec/installation_spec.rb
+++ spec/installation_spec.rb
@@ -6,19 +6,47 @@ end
 
 describe 'redis Ansible role' do
 
-if ['debian', 'ubuntu'].include?(os[:family])
-    describe 'Specific Debian and Ubuntu family checks' do
+    # Define variables
+    packages = Array[]
+    process_name = ''
+    process_user = ''
+    service_name = ''
+    user_home = ''
 
-        it 'install role packages' do
-            packages = Array[ 'apt' ]
-
-            packages.each do |pkg_name|
-                expect(package(pkg_name)).to be_installed
-            end
-        end
-
+    if ['debian', 'ubuntu'].include?(os[:family])
+        packages = Array[ 'redis-server' ]
+        process_name = 'redis-server'
+        process_user = 'redis'
+        service_name = 'redis-server'
+        user_home = '/var/lib/redis'
     end
-end
 
+    # Begin tests
+    it 'install role packages' do
+        packages.each do |pkg_name|
+            expect(package(pkg_name)).to be_installed
+        end
+    end
+
+    describe user(process_user) do
+        it { should exist }
+        it { should belong_to_group process_user }
+        it { should have_home_directory user_home }
+        it { should have_login_shell '/bin/false' }
+    end
+
+    describe port('6379') do
+        it { should be_listening }
+    end
+
+    describe process(process_name) do
+        it { should be_running }
+        its(:user) { should eq process_user }
+    end
+
+    describe service(service_name) do
+        it { should be_enabled }
+        it { should be_running }
+    end
 end
 
